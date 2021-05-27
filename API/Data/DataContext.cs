8add7281c7b6ff7e227ef032ccadf3ed7cb2f95b
Casper J <casper.jaller@gmail.com>
2021-05-27 10:27:25 +0200

    fourth commit: DataContext populated

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,9 +1,15 @@
+using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class DataContext : DbContext
     {
-        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 }
